a343b1db20220039022000d3630512300dcec9b3
Santzu-27 <gabi260402@gmail.com>
2022-10-11 23:19:13 -0300

    next page

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
 function renderMainPage(){
-    const mainP = document.querySelector('.start')
-    mainP.classList.remove('start')
-    mainP.classList.add('render')
+    const mainP = document.querySelector('#main--screen')
+    mainP.style.opacity = '1'
 }
 renderMainPage()
 
-function renderApp(){
-    let initialScreen = document.querySelectorAll('.render')
-    console.log(initialScreen)
-    for (let i = 0; i < initialScreen.length; i++){
-        initialScreen[i].classList.add('animate__animated', 'animate__slideOutUp')
-    }
-
-    let secondPage = document.querySelector('#section--second-page')
-    secondPage.style.display = 'block'
-    secondPage.classList.remove('in-bottom')
-    secondPage.classList.add('to-top')
-    // secondPage.classList.add('animate__animated', 'animate__slideInUp', 'to-top')
+function nextPage(){
+    let page1 = document.querySelector("#section--introduction")
+    let page2 = document.querySelector("#section--second-page")
+    page1.style.top = '-800px'
+    page2.style.top = '0'
 }
