4dc6373c318536dc65840eef34fcd9ac5f4bc40c
Santzu-27 <gabi260402@gmail.com>
2022-12-15 12:19:40 -0300

    tests

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 let page1 = document.querySelector("#section--introduction")
 let page2 = document.querySelector("#section--second-page")
-let footer = document.getElementsByTagName('footer')
+let pageApp = document.querySelector('#application')
 let delay_ms = 26
+let delayDisplay = 500
+renderMainPage()
 
 document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
@@ -13,10 +15,44 @@ document.addEventListener('DOMContentLoaded', function() {
 function renderMainPage(){
     const mainP = document.querySelector('#main--screen')
     mainP.style.opacity = '1'
-    
+    createButtons()
+}
+
+function createButtons() {
+    let button1 = document.querySelector('#button1');
+    let buttonStart = document.querySelector('#button-start');
+
+    button1.addEventListener('click', nextPage);
+    buttonStart.addEventListener('click', startApp);
 }
 
 function nextPage(){
     page1.style.top = '-800px'
     page2.style.top = '0'
+}
+
+function startApp(){
+    let mainScreen = document.querySelector('#main--screen')
+    mainScreen.style.transition = 'opacity .5s, display 2s'
+    hideMain()
+    mainScreen.style.opacity = 0
+}
+
+function hideMain(){
+    let mainScreen = document.querySelector('#main--screen')
+    let firstFooter = document.querySelector('#first--footer')
+    setTimeout(function(){
+        pageApp.style.display = 'block'
+        
+        mainScreen.style.display = 'none'
+        mainScreen.innerHTML = ''
+        
+        document.body.style.overflow = 'scroll'
+        
+        pageApp.style.opacity = 1
+        pageApp.style.zIndex = 2
+
+        firstFooter.style.display = 'none'
+        firstFooter.innerHTML = ''
+    }, delayDisplay)
 }
--- test/test.js
+++ test/test.js
@@ -1,32 +1,20 @@
-let buttontop = document.getElementById('buttontest')
-buttontop.addEventListener('click', scrollWindow)
-window.scrollTo(0, 0)
+const buttons = []
+const values = {
+    value 
+}
+document.addEventListener('DOMContentLoaded', getButtons)
 
-window.onbeforeunload =teste1
-window.onload = scrollWindow
-window.onunload =teste3
-
-document.onbeforeunload =teste4
-document.onload =teste5
-document.onunload =teste6
-
-function avancar(){
-    let first = document.getElementById('first')
-    let second = document.getElementById('second')
-    console.log(first)
-    first.style.top = '-500px';
-    second.style.opacity = '1'
-    second.style.top = '0'
+function getButtons (){
+    for (var i=0; i<=5 ; i++){
+        buttons[i] = document.querySelector(`#bt${i+1}`)
+    }
+    startButtons()
 }
 
-function scrollWindow(){
-    window.scrollTo(0, 0) 
-    console.log('funcionou')
+function startButtons(){
+    for (let i=0; i<=5 ; i++){
+        buttons[i].addEventListener('click',{
+            
+        })
+    }
 }
-
-function teste1 (){console.log ('teste1')}
-function teste2 (){console.log ('teste2')}
-function teste3 (){console.log ('teste3')}
-function teste4 (){console.log ('teste4')}
-function teste5 (){console.log ('teste5')}
-function teste6 (){console.log ('teste6')}
